513e4101a4e3b4593f2a59cac942a28725cd00fa
sarava338 <sarava338@gmail.com>
2023-05-20 14:27:27 +0530

    Added dotenv package

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const submitEvent = (e) => {
   if (!city.value) alert("Enter Input Details Correctly");
   else
     getData(
-      `https://api.weatherapi.com/v1/forecast.json?key=a50a2f9209c04431921163312220806&q=${city.value}&days=1&aqi=no&alert=no`
+      `https://api.weatherapi.com/v1/forecast.json?key=${process.env.API_KEY}&q=${city.value}&days=1&aqi=no&alert=no`
     );
   city.value = "";
 };
